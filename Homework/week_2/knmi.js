5dd5cd243e5cab8f8717c9c1786b16789ae8a757
HNLe <hy.nhu.le@gmail.com>
2017-11-10 02:29:22 +0100

    Werkt soortvan

--- Homework/week_2/knmi.js
+++ Homework/week_2/knmi.js
@@ -7,10 +7,10 @@ var data2 = data.split('\n');
 
 // Make empty arrays to store date and temp
 var date = [];
-var maxTemp = [];
+var temperature = [];
 
 // Split data into date and max temperature
-for (var i = 2; i < data2.length; i++) {
+for (var i = 2; i < data2.length - 1; i++) {
     var singleDate = data2[i].split(",");
 
     // console.log(singleDate[0]);
@@ -28,27 +28,127 @@ for (var i = 2; i < data2.length; i++) {
     // console.log("String Date", formattedDate);
 
     date.push(new Date(formattedDate));
-    maxTemp.push(singleDate[1]);
+    temperature.push(Number(singleDate[1]));
 }
 
-console.log("date", date);
-console.log("maxTemp", maxTemp);
+console.log("Date ", date);
+console.log("Temperature ", temperature);
 
-canvas();
+var xCoordinate = [];
+var yCoordinate = [];
 
-var domain = [];
-var range = [];
+// Source: https://johnresig.com/blog/fast-javascript-maxmin/
 
-function canvas(){
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
+Array.min = function( temperature ){
+    return Math.min.apply( Math, temperature );
+};
 
-    ctx.moveTo(0,0);
-    ctx.lineTo(600,300);
-    ctx.stroke();
+Array.max = function( temperature ){
+    return Math.max.apply( Math, temperature );
+};
+
+var minTemp = Array.min(temperature);
+var maxTemp = Array.max(temperature);
+
+// var domainTemp = [minTemp, maxTemp];
+var domainTemp = [maxTemp, minTemp];
+console.log("DomainTemp", domainTemp);
+
+
+console.log("min: " + minTemp + " max: " + maxTemp);
+
+var space = 90;
+// To move the axe away from the edge
+var whiteSpace = 40;
+
+var test = (40+ 1460)/13;
+
+var rangeX = [test, 1500];
+var rangeY = [190, 820];
+
+console.log("DomainTemp: ", domainTemp);
+console.log("rangeY: ", rangeY);
+
+for(var i = 0; i < temperature.length; i++){
+
+    var singleTempPoint = (createTransform(domainTemp, rangeY)(temperature[i]));
+    // console.log("Point: ", point);
+    yCoordinate.push(singleTempPoint);
+}
+
+console.log("yCoordinate: ", yCoordinate);
+console.log("length: ", yCoordinate.length);
+
+var dateDays = [];
+var daysSinceJanuary = [];
+
+for (i = 0; i < date.length; i++){
+    var dataDate = new Date(date[i]);
+    var dataInMilliSeconds = Number(dataDate.getTime(date[i]));
+    var dataIndays = (dataInMilliSeconds / 86400000);
+    daysSinceJanuary.push(dataIndays);
+    var axeDate = (dataIndays - daysSinceJanuary[0]);
+    dateDays.push(axeDate);
 }
 
 
+console.log("Days: ", dateDays);
+
+var domainDate = [dateDays[0], dateDays[364]];
+for(var i = 0; i < dateDays.length; i++){
+
+    var singleDatePoint = (createTransform(domainDate, rangeX)(dateDays[i]));
+    xCoordinate.push(singleDatePoint);
+}
+
+console.log("xCoordinate: ", xCoordinate);
+
+Array.min = function( xCoordinate ){
+    return Math.min.apply( Math, xCoordinate );
+};
+
+Array.max = function( xCoordinate ){
+    return Math.max.apply( Math, xCoordinate );
+};
+
+var minCor = Array.min(xCoordinate);
+var maxCor = Array.max(xCoordinate);
+
+console.log("MIN: " + minCor + " MAX " + maxCor);
+
+var canvas = document.getElementById("canvas");
+var graph = canvas.getContext("2d");
+var sectionDate;
+
+drawAxes();
+
+draw();
+
+// for (i = 0; i < 364; i++){
+//     console.log("xCoordinate: " + xCoordinate[i] + " yCoordinate: " + yCoordinate[i]);
+// }
+
+function draw(){
+
+    for(i = 0; i < date.length; i++){
+
+        graph.beginPath();
+        graph.moveTo(xCoordinate[i], yCoordinate[i]);
+        graph.lineTo(xCoordinate[i+1], yCoordinate[i+1]);
+
+        graph.lineWidth = 2;
+
+        graph.stroke();
+        graph.strokeStyle="#ff6666";
+
+        // console.log("MOVE TO) Coordinaten X: " + start + " Coordinaten Y: " + prev_point);
+        // console.log('\n');
+        // console.log("LINE TO) Coordinaten X: " + start + " Coordinaten Y: " + prev_point);
+    }
+
+
+
+}
 
 function createTransform(domain, range){
 	// domain is a two-element array of the data bounds [domain_min, domain_max]
@@ -56,7 +156,7 @@ function createTransform(domain, range){
 	// this gives you two equations to solve:
 	// range_min = alpha * domain_min + beta
 	// range_max = alpha * domain_max + beta
- 		// a solution would be:
+ 	// a solution would be:
 
     var domain_min = domain[0]
     var domain_max = domain[1]
@@ -71,4 +171,92 @@ function createTransform(domain, range){
     return function(x){
       return alpha * x + beta;
     }
+}
+
+function drawAxes(){
+
+    var canvasWidth = 1600;
+    var canvasHeight = 860;
+
+    // 900 - 90 = 810
+    // 1500 - 40 = 1460
+    // 1460 / 12
+
+    var sectionDate = (40+ 1460)/13;
+
+    // // Horizontal Axe
+    // graph.beginPath();
+    // graph.moveTo(whiteSpace, (canvasHeight - space));
+    // graph.lineTo(1500, (canvasHeight - space));
+    // graph.stroke();
+    //
+    graph.fillText('TEST', 50, 860);
+    graph.fillText('TEST', sectionDate, 860);
+    graph.fillText('HOOGTE', 300, 40);
+    graph.fillText('HOOGTE', 300, 860);
+
+    // Labels
+
+    // Vertical Axe
+    graph.beginPath();
+
+    // xAxis = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", ""];
+    xAxis = ["Nov (2016)", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov(2017)"];
+
+    // 40
+    // 900 - 40 = 860
+    // 860 - 40 = 820
+    // 930 - 40 40 30
+    // 930 - 110 = 820 /
+
+    for (i = 1; i < 14; i++){
+        graph.moveTo((i * sectionDate), whiteSpace);
+        graph.lineTo((i * sectionDate), (canvasHeight - whiteSpace));
+        graph.stroke();
+        graph.fillText(xAxis[i-1], (i * sectionDate), 840);
+
+        // Make only the first line black
+        if(i == 1){
+            graph.beginPath();
+            graph.strokeStyle="#000000";
+        }
+        else{
+            graph.strokeStyle="#d3d3d3";
+        }
+    }
+
+
+    var sectionTemp = 820/7;
+    graph.beginPath();
+
+    // var yAxis = ["", "-50", "0", "50", "100", "150", "200", "250", "300", "350"];
+    var yAxis = ["250", "200", "150", "100", "50", "0", "-50"];
+
+    // 900 - 90
+    // Horizontal
+    // 70
+    // 860, 790
+    for (i = 0; i < 8; i++){
+        graph.moveTo(whiteSpace, (canvasHeight - (sectionTemp * i)));
+        graph.lineTo(1500, (canvasHeight - (sectionTemp * i)));
+        graph.stroke();
+        graph.fillText(yAxis[i-1], space, (i * sectionTemp + whiteSpace ));
+
+        if(i == 0){
+            graph.beginPath();
+            graph.strokeStyle="#000000";
+        }
+        else {
+            graph.beginPath();
+            graph.strokeStyle="#d3d3d3";
+        }
+    }
+
+
+
+
+
+
+
+
 }
