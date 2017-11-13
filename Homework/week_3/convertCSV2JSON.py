fd96e6577164f35af8ea8f69a5e076dddddfe0b5
HNLe <hy.nhu.le@gmail.com>
2017-11-13 21:31:26 +0100

    Maandag

--- Homework/week_3/convertCSV2JSON.py
+++ Homework/week_3/convertCSV2JSON.py
@@ -9,10 +9,35 @@
 #
 # STN,YYYYMMDD,   RH
 
+#
 #  {
-#   "Month"
+#   "Month":[
 #       "Date"
 #       "Amount"
+#       ],
 #  }
 #
-#
+
+# Save it into JSON file
+
+import csv
+import json
+
+# Load in CSV
+f = open('knmi.csv', 'r')
+csv_f = csv.reader(f);
+
+testList = []
+for row in csv_f:
+    date = row[0]
+    dateFormatted = date[0:4] + '-' + date[4:6] + '-' + date[6:8]
+    print dateFormatted
+    amount = row[1]
+    data = { 'date':dateFormatted, 'amount':amount }
+    testList.append(data)
+
+# Reformat data into JSON
+with open('knmi.json', 'w') as fp:
+    json.dump(testList, fp)
+
+fp.close
