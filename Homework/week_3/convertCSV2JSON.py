0c4c46d41e52c2ee30eed68af78ee386a742ac64
HNLe <hy.nhu.le@gmail.com>
2017-11-13 14:32:25 +0100

    Add CSV file

---
+++ Homework/week_3/convertCSV2JSON.py
@@ -0,0 +1,18 @@
+# Tiny Le
+# 11130717
+
+
+# Covert into KNMI text file into JSON format
+
+# YYYYMMDD = Datum (YYYY=jaar MM=maand DD=dag);
+# RH       = Etmaalsom van de neerslag (in 0.1 mm) (-1 voor <0.05 mm);
+#
+# STN,YYYYMMDD,   RH
+
+#  {
+#   "Month"
+#       "Date"
+#       "Amount"
+#  }
+#
+#
